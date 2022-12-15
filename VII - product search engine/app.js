4706746dcd73272f0bc2f5f33cfe6d8497f46600
decapacitator <anushkabishnoi9@gmail.com>
2022-12-15 17:41:59 +0530

    interactive doneee

--- VII - product search engine/app.js
+++ VII - product search engine/app.js
@@ -2,7 +2,7 @@ const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
   const storeitems = document.getElementById("product-list");
   const product = document.querySelectorAll(".product ");
-  const pname = document.getElementsByTagName("h2");
+  const pname = storeitems.getElementsByTagName("h2");
 
   for (var i = 0; i < pname.length; i++) {
     let match = product[i].getElementsByTagName("h2")[0];
